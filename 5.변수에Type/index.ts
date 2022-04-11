4a934d480ae0c8b0a007d40da02b7ad49e13bcd5
chry8822 <goggg8822@naver.com>
2022-04-11 14:54:33 +0900

    edit type alias

--- 5.변수에Type/index.ts
+++ 5.변수에Type/index.ts
@@ -52,3 +52,78 @@ type NewType = PositionX & PositionY ;
 
 
 
+// object 타입을 정의한 type alias 두개를 & 기호로 합칠 때 중복된 속성이 있으면 어떻게 될까요??
+
+type One = { name : string }
+type Two = { name : string }
+
+type Three = One & Two;
+
+let four : Three = { name : "kim"};
+
+
+// 다음 조건을 만족하는 타입을 만들어봅시다. 
+// 1. 이 타입은 object 자료형이어야합니다.
+
+// 2. 이 타입은 color 라는 속성을 가질 수도 있으며 항상 문자가 들어와야합니다. 
+
+// 3. 이 타입은 size 라는 속성이 있어야하며 항상 숫자가 들어와야합니다.
+
+// 4. 이 타입은 position 이라는 변경불가능한 속성이 있어야하며 항상 숫자가 담긴 array 자료가 들어와야합니다.  
+
+
+type Obj = { 
+  color?: string, 
+  size : number,
+  readonly position : number[],
+}
+
+let obj :Obj  = {
+  size : 20,
+  position : [1,2,3]
+}
+
+
+// (숙제3) 다음을 만족하는 type alias를 연습삼아 간단히 만들어보십시오. 
+
+// 1. 대충 이렇게 생긴 object 자료를 다룰 일이 많습니다. { name : 'kim', phone : 123, email : 'abc@naver.com' }
+
+// 2. object 안에 있는 이름, 전화번호, 이메일 속성이 옳은 타입인지 검사하는 type alias를 만들어봅시다.
+
+// 3. 각 속성이 어떤 타입일지는 자유롭게 정하십시오. 
+
+type Obj2 = {
+  name : string,
+  phone : number,
+  email : string,
+}
+
+let obj2 :Obj2 = {
+  name : "oh",
+  phone : 9134111,
+  email : "gogogo",
+}
+
+
+
+// (숙제4). 다음을 만족하는 type alias를 만들어보십시오.
+
+// 1. 숙제2와 똑같은데 이번엔 이름, 전화번호, 이메일, 미성년자여부 속성을 옳은 타입인지 검사하는 type alias를 만들어봅시다.
+
+// 2. 미성년자 여부 속성은 true/false만 들어올 수 있습니다. 
+
+// 3. 멋있게 숙제2에서 만들어둔  type alias를 재활용해봅시다.
+
+type Obj3 = {
+  adult : boolean,
+}
+
+type Obj4 = Obj2 & Obj3;
+
+let obj3 :Obj4 = {
+  name : "oh",
+  phone : 9134111,
+  email : "gogogo",
+  adult : true
+}
+
